677379263538e7796f26e26f8498214260d22ec6
bcq <3488613264@qq.com>
2022-12-31 10:19:55 +0800

    fix: add wcfg config

--- vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
+++ vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Dec 31 10:11:39 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Dec 31 10:19:31 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "69ea7768-898f-4802-8b64-6bb209496add" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "487c187e6bc74760aff3f9efe82821de" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "19" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "20" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "12th Gen Intel(R) Core(TM) i7-12700" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8804_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2750710621 -regid "" -xml C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8808_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 615389500 -regid "" -xml C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/moona/Desktop/CO-lab-material-CQU/vivado/lab4/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/tb_top_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
